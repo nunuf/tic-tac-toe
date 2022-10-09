798740bec2499b35e3eacdd335a80cfc1ae1d2b5
nunuf <nofarcheka92@gmail.com>
2022-10-09 14:06:12 +0300

    Fix font for mobile

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Game from './Components/Game/Game';
 import reportWebVitals from './reportWebVitals';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(
